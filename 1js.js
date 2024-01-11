172e50acc8fb45be27012cccc37687dde507ac19
priyaagrawal123 <147872840+priyaagrawal123@users.noreply.github.com>
2024-01-11 19:27:09 +0530

    Add files via upload

---
+++ 1js.js
@@ -0,0 +1,45 @@
+console.log("hello priya");
+console.log("my first js code!");
+fullName ="priya"
+console.log(fullName)
+
+//Datatypes
+let age=27; //number
+var number=20.5;    //number
+let name="priya" //string
+const city=true   //boolean
+let x; //undefined
+let y=null; //null
+let wood=BigInt("123"); //BigInt
+let maths=Symbol("hello!"); //Symbol
+
+//objects
+const student={
+    name:"priya",
+age:12,
+
+}    
+;
+
+//Q. create a const object called "product" to store information shown in the picture
+
+const product={
+    name:"glitter",
+    Rating: 4,
+    price:50,
+    offer:5,
+};
+
+//strings
+"123"+1 //"1231"
+"12"+"1234" //121234
+
+//Q. create a const object called "profile" to store information
+const profile={
+    name:"priya",
+    followers:134,
+    isfollow:true,
+    posts:17688,
+};
+console.log(typeof profile(name));
+console.log(typeof profile["name"]);
