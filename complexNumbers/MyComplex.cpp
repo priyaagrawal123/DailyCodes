ae159f187f07f3da035df43fd056af2496e174cd
priyaagrawal123 <147872840+priyaagrawal123@users.noreply.github.com>
2024-01-25 13:50:49 +0530

    Add files via upload

---
+++ complexNumbers/MyComplex.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "MyComplex.hpp"
+using namespace std;
+MyComplex::MyComplex()
+{
+    real = 0;
+    img = 0;
+}
+void MyComplex::print()
+{
+    cout << real << "+" << img << "i";
+}
+void MyComplex::add(MyComplex c1, MyComplex c2)
+{
+    real = c1.real + c2.real;
+    img = c1.img + c2.img;
+}
