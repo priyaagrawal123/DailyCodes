08747bbfb27c0f84ea1902499e6ce94144b83c68
priyaagrawal123 <147872840+priyaagrawal123@users.noreply.github.com>
2024-02-15 22:20:09 +0530

    Add files via upload

---
+++ Math.cpp
@@ -0,0 +1,30 @@
+// Addition
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int x = 45;
+    // int y = 56;
+    // int z = x + y;
+
+    // cout << "\nTotal:" << z << endl;
+
+    // return 0;
+
+    // double x = 45;
+    // int y = 5;
+    // double z = x + y;
+
+    // cout << "\n Total: " << z << endl;
+
+    int x = 10;
+    int y = x++; // x = 11 and y = 10
+
+    // cout << "Incre:" << x << endl;
+    cout << "Y:" << y << endl;
+
+    return 0;
+}
