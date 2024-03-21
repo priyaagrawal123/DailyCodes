20295b9a197b42114ba99ef3e76178085928d579
priyaagrawal123 <147872840+priyaagrawal123@users.noreply.github.com>
2024-03-21 22:58:07 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,7 @@
+console.log("i m".length);  //ans: 3 //length= it counts number and spaces also
+console.log("remove tension ko bye bye".toUpperCase());  //methodName()  example:-toUpperCase,trim()
+console.log(Math.random()); //it gives random bet 0 and 1
+console.log(Math.random() * 50); //0 and 50
+console.log(Math.floor(Math.random() * 50));  //nearset whole number 
+console.log(Math.ceil(40));   //finds the smallest integer grater than or equal to 40 
+//for more refer mdn built in objects
