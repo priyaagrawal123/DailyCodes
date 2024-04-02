189cec009b36c75bd45214cd99f24341f653c5ad
priyaagrawal123 <147872840+priyaagrawal123@users.noreply.github.com>
2024-04-02 18:46:39 +0530

    Add files via upload

---
+++ tests.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float num1=1.1;
+    double num2=1.1;
+    if(num1==num2)
+    cout<<"standorf";
+    else
+    cout<<"harvard";
+    return 0;
+}
